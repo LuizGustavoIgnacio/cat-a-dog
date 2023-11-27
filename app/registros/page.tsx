1bfd427b8db1f3e61a677d52f128365944ccb07a
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-11-27 20:10:56 -0300

    list pagination

--- app/registros/page.tsx
+++ app/registros/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
     <div className="flex">
       <UserMenu />
 
-      <div className="bg-[#23222F] w-full h-[100vh] py-[45px] px-[40px]">
+      <div className="bg-[#23222F] min-h-[100vh] max-h-max w-full py-[45px] px-[40px]">
         <div className="flex flex-col space-y-24">
           <div className="flex flex-col space-y-5">
             <RecordsTable />
